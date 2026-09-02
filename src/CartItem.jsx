149a14b9f5aefc59bd843dfb7a007629a00e9d3c
Ahmed <anotheronebitingthedust@proton.me>
2026-09-02 10:19:58 -0400

    Implement CartSlice reducers and export actions

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,7 +7,6 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
     let total = 0;
     cart.forEach(item => {
@@ -21,7 +20,8 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
-  dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
@@ -30,12 +30,12 @@ const CartItem = ({ onContinueShopping }) => {
     } else {
       dispatch(removeItem(item.name));
     }
+  };
 
   const handleRemove = (item) => {
     dispatch(removeItem(item.name));
   };
 
-  // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
     const costNumber = parseFloat(item.cost.replace('$', ''));
     return (costNumber * item.quantity).toFixed(2);
@@ -70,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
